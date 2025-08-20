bd689f381c9f89a63de126cc4f70b2cc2cd63391
Tracy Huang <chiayu561@g.ucla.edu>
2025-08-20 15:32:18 -0700

    fix: check if read() failed

--- TCP/io.c
+++ TCP/io.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <sys/fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
 void init_io() {
     int flags = fcntl(STDIN_FILENO, F_GETFL);
@@ -9,8 +12,16 @@ void init_io() {
 }
 
 ssize_t input_io(uint8_t* buf, size_t max_length) {
-    ssize_t len = read(STDIN_FILENO, buf, max_length);
-    return len > 0 ? len : 0;
+    ssize_t len = read(STDIN_FILENO, buf, max_length); 
+    
+    if (len < 0) {   
+        if (errno == EAGAIN || errno == EWOULDBLOCK){ 
+            return 0; // no data available at STDIN yet
+        }
+        fprintf(stderr, "[ERROR] read() failed to read data from STDIN.\n");
+        exit(1);
+    }
+    return len;
 }
 
 void output_io(uint8_t* buf, size_t length) {
